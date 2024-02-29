2c4265886670b27637d8db8799de9e871eb3760d
Salma Torres <tsalma569@gmail.com>
2024-02-29 09:54:42 -0400

    Se aplica un impuesto de 8.25% si el estado es CA

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,8 +1,18 @@
-function calcularprecio(a, b) {
+function calcularprecio(a, b, c) {
     const cantidad = a;
     const precio = b;
-    var precio_neto = cantidad*precio;
-    return precio_neto;
+    const estado =c;
+    const precio_neto = cantidad*precio;
+    var impuesto;
+    switch(estado){
+      case "CA": 
+        impuesto = precio_neto*8.25/100;
+        break;
+      default:
+        impuesto = 0;
+    }
+    const precio_final = precio_neto+impuesto;
+    return precio_final;
   }
   
   export default calcularprecio;
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -12,4 +12,7 @@ describe("Totalizador", () => {
   it("Devolver precio neto", () => {
     expect(calcularprecio(2,5)).toEqual(10);
   });
+  it("Si el estado es CA si aplica un impuesto de 8.25%", () => {
+    expect(calcularprecio(20,3,"CA")).toEqual(64.95);
+  });
 });
