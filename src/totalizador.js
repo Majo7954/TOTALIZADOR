b6c65ef6c8f5b326587dacbe8ccd53ce6436962d
Maria Jose Murillo <murillomendoza.mariajose@gmail.com>
2024-02-29 09:40:03 -0400

    Devolver Precio Neto

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,7 +1,8 @@
-function calcularprecio(a) {
+function calcularprecio(a, b) {
     const cantidad = a;
-    const precio = a;
-    return precio;
+    const precio = b;
+    var precio_neto = cantidad*precio;
+    return precio_neto;
   }
   
   export default calcularprecio;
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -2,10 +2,14 @@ import calcularprecio from "./totalizador.js";
 
 describe("Totalizador", () => {
   it("Devolver cantidad ingresada", () => {
-    expect(calcularprecio(2)).toEqual(2);
+    expect(calcularprecio(2,1)).toEqual(2);
   });
-  
+
   it("Devolver precio ingresado", () => {
-    expect(calcularprecio(2)).toEqual(2);
+    expect(calcularprecio(1,2)).toEqual(2);
+  });
+
+  it("Devolver precio neto", () => {
+    expect(calcularprecio(2,5)).toEqual(10);
   });
 });
